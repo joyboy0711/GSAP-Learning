16d9494ed21483abad828fa18af6a5de1fb16560
joyboy0711 <freedomchaserjb@gmail.com>
2025-09-03 19:02:51 +0530

    I have understand the basic properties of GSAP

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,16 @@
+import {gsap} from "gsap";
+
+gsap.to(".box", {
+    opacity: 1,
+    duration: 2,
+    rotation: 360,
+    background: '#ff6f61',
+    borderRadius: '50%',
+    scale: 1.2,
+    x: 600,
+    delay: 0.5,
+    ease: 'power2.inOut',
+    yoyo: true,
+    repeat: -1,
+    repeatDelay: 0.2
+});
